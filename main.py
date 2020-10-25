ff59c72c87500e428cf900c4393b1e44f7fb51a3
MelvinWolf <melvin.wolf.toulouse@gmail.com>
2020-10-25 14:43:18 +0100

    Update

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from textblob import TextBlob
-
+import nltk
+nltk.download('punkt')
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
@@ -16,4 +17,6 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-book = open("The Hunger Games.txt")
+book = str(open("The Hunger Games.txt"))
+blob = TextBlob(book)
+print(blob.tags)
